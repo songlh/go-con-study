41085a4d071f434fd2fcaba65f2539ef0b5665d9
linhai.song <songlinhai0543@gmail.com>
2018-06-01 14:22:21 -0700

    change bug file

--- etcd/deadlock/bugs/etcd5349/bug5349_election_test.go
+++ etcd/deadlock/bugs/etcd5349/bug5349_election_test.go
@@ -14,18 +14,18 @@ func TestElectionSessionRecampaignBug5349(t *testing.T) {
 	defer clus.Terminate(t)
 	cli := clus.RandClient()
 
-	e := concurrency.NewElectionBug(cli, "test-elect")
-	if err := e.CampaignBug(context.TODO(), "abc"); err != nil {
+	e := concurrency.NewElection(cli, "test-elect")
+	if err := e.Campaign(context.TODO(), "abc"); err != nil {
 		t.Fatal(err)
 	}
-	e2 := concurrency.NewElectionBug(cli, "test-elect")
-	if err := e2.CampaignBug(context.TODO(), "def"); err != nil {
+	e2 := concurrency.NewElection(cli, "test-elect")
+	if err := e2.Campaign(context.TODO(), "def"); err != nil {
 		t.Fatal(err)
 	}
 
 	ctx, cancel := context.WithCancel(context.TODO())
 	defer cancel()
-	if resp := <-e.ObserveBug(ctx); len(resp.Kvs) == 0 || string(resp.Kvs[0].Value) != "def" {
+	if resp := <-e.Observe(ctx); len(resp.Kvs) == 0 || string(resp.Kvs[0].Value) != "def" {
 		t.Fatalf("expected value=%q, got response %v", "def", resp)
 	}
 }
